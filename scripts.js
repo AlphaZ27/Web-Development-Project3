9de7a60a12a19f8954f2bab5674309ab60f5702c
AlphaZ27 <166666998+AlphaZ27@users.noreply.github.com>
2024-07-09 11:13:54 -0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,59 @@
+//   Image Gallery 
+
+  var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("trans");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
+
+
+ 
+//  3 Button Tabs | Show/Hide Function
+function selectTab(tabIndex) {
+  //Hide All Tabs
+  document.getElementById('tab1Content').style.display="none";
+  document.getElementById('tab2Content').style.display="none";
+  document.getElementById('tab3Content').style.display="none";
+  
+  //Show the Selected Tab
+  document.getElementById('tab' + tabIndex + 'Content').style.display="block";  
+}
+
+
+
+//  Form Validated against empty submission
+function validateForm() {
+  var w = document.forms["myForm"]["fname"].value;
+  x = document.forms["myForm"]["lname"].value;
+  y = document.forms["myForm"]["email"].value;
+  z = document.forms["myForm"]["message"].value;
+  if (w == "" && x == "" && y == "" && z == "") {
+    alert("All information is required for submission");
+    return false;
+  }
+}
